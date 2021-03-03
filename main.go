8f8ac90adefe71ad92c339c2161eb6b6898357bc
Trevor Sawler <trevor.sawler@gmail.com>
2021-03-03 10:40:15 -0400

    Comments

--- main.go
+++ main.go
@@ -8,17 +8,28 @@ import (
 )
 
 func main() {
+	// reader is a variable used to read whatever the user types in on the console
 	reader := bufio.NewReader(os.Stdin)
 
+	// print the welcome message and instructions to the screen
 	fmt.Println(fmt.Sprintf("%s %s %s", "\033[1m", Intro(), "\033[0m"))
 
+	// execute everything in this loop until the user enters 'quit'
 	for {
+		// print out a prompt
 		fmt.Print("-> ")
+
+		// wait for the user to type something and press enter
 		userInput, _ := reader.ReadString('\n')
+
+		// strip the carriage return off of whatever they typed
 		userInput = strings.Replace(userInput, "\n", "", -1)
+
 		if userInput == "quit" {
+			// end program
 			break
 		} else {
+			// go buld the response and write it to the console
 			resp := fmt.Sprintf("%s%s%s", "\033[1m", Response(userInput), "\033[0m")
 			fmt.Println(resp)
 		}
