d9e4890c24db397fce324333775d556a726e96b2
Trevor Sawler <trevor.sawler@gmail.com>
2021-03-03 13:43:21 -0400

    Check for case on pronouns in matches

--- main.go
+++ main.go
@@ -9,20 +9,23 @@ import (
 
 func main() {
 	// reader is a variable used to read whatever the user types in on the console
+	// reader requires some input source (anything that satisfies the io.Reader interface),
+	// and in this case we'll use "standard in" (the keyboard/console).
 	reader := bufio.NewReader(os.Stdin)
 
-	// print the welcome message and instructions to the screen
+	// Print the welcome message and instructions to the screen. Note that we used a function as a parameter
+	// to another function. Go has first class functions.
 	fmt.Println(Intro())
 
-	// execute everything in this loop until the user enters 'quit'
+	// Execute everything in this loop until the user enters 'quit' and presses return.
 	for {
-		// print out a prompt
+		// Print out a prompt
 		fmt.Print("-> ")
 
-		// wait for the user to type something and press enter
+		// Wait for the user to type something and press enter
 		userInput, _ := reader.ReadString('\n')
 
-		// strip the carriage return off of whatever they typed
+		// Strip the carriage return off of whatever they typed
 		userInput = strings.Replace(userInput, "\n", "", -1)
 
 		if userInput == "quit" {
